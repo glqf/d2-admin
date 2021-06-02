2a1070edfbc98ff2cccee10b690ed3e7a5b3811d
FairyEver <1711467488@qq.com>
2021-06-02 15:53:41 +0800

    refactor: :construction: archive

--- src/views/example/button/loading.vue
+++ src/views/example/button/loading.vue
@@ -15,7 +15,7 @@
     <d2-button icon-right="heroicons-solid:menu" :loading-right="loading">button</d2-button>
   </the-section>
   <the-section title="circle">
-    <d2-button :loading="loading" circle>button</d2-button>
+    <d2-button :loading="loading" icon="mdi:hand-right" circle/>
   </the-section>
   <the-section title="disabled">
     <d2-button :loading="loading" disabled>button</d2-button>
@@ -25,6 +25,10 @@
   </the-section>
   <the-section title="round">
     <d2-button :loading="loading" round>button</d2-button>
+  </the-section>
+  <the-section title="round half icon">
+    <d2-button icon="akar-icons:chevron-left" :loading="loading" round-left>Left</d2-button>
+    <d2-button icon-right="akar-icons:chevron-right" :loading-right="loading" round-right>Right</d2-button>
   </the-section>
   <the-section title="text">
     <d2-button :loading="loading" text>button</d2-button>
