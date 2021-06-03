59ba93be8a169560c2c06d0d3e4b4e5f3cb6164c
FairyEver <1711467488@qq.com>
2021-06-03 20:33:53 +0800

    refactor: :construction: archive

--- src/lib/d2-components/packages/D2Button/src/index.vue
+++ src/lib/d2-components/packages/D2Button/src/index.vue
@@ -29,31 +29,33 @@ import {
 } from '../../../utils/helper.js'
 import D2Icon from '../../D2Icon/src/index.vue'
 
+export const buttonProps = {
+  icon: { type: String, default: '' },
+  iconRight: { type: String, default: '' },
+  color: { type: String, default: '', validator: value => !value || isValidColor(value) },
+  size: { type: String, default: '', validator: value => !value || isValidSize(value) },
+  type: { type: String, default: 'button', validator: value => isValidButtonTypes(value)},
+  autofocus: { type: Boolean, default: false },
+  text: { type: Boolean, default: false },
+  disabled: { type: Boolean, default: false },
+  loading: { type: Boolean, default: false },
+  loadingRight: { type: Boolean, default: false },
+  plain: { type: Boolean, default: false },
+  round: { type: Boolean, default: false },
+  roundLeft: { type: Boolean, default: false },
+  roundRight: { type: Boolean, default: false },
+  circle: { type: Boolean, default: false },
+  ring: { type: Boolean, default: false },
+  ringOffset: { type: [Boolean, Number], default: false, validator: value => isBoolean(value) || isIntegerAndBetween(value, 0, 4) },
+  ringWidth: { type: Number, default: 2 }
+}
+
 export default {
   name: 'D2Button',
   components: {
     D2Icon
   },
-  props: {
-    icon: { type: String, default: '' },
-    iconRight: { type: String, default: '' },
-    color: { type: String, default: '', validator: value => !value || isValidColor(value) },
-    size: { type: String, default: '', validator: value => !value || isValidSize(value) },
-    type: { type: String, default: 'button', validator: value => isValidButtonTypes(value)},
-    autofocus: { type: Boolean, default: false },
-    text: { type: Boolean, default: false },
-    disabled: { type: Boolean, default: false },
-    loading: { type: Boolean, default: false },
-    loadingRight: { type: Boolean, default: false },
-    plain: { type: Boolean, default: false },
-    round: { type: Boolean, default: false },
-    roundLeft: { type: Boolean, default: false },
-    roundRight: { type: Boolean, default: false },
-    circle: { type: Boolean, default: false },
-    ring: { type: Boolean, default: false },
-    ringOffset: { type: [Boolean, Number], default: false, validator: value => isBoolean(value) || isIntegerAndBetween(value, 0, 4) },
-    ringWidth: { type: Number, default: 2 }
-  },
+  props: buttonProps,
   emits: [
     'click'
   ],
--- src/lib/d2-components/packages/D2ButtonGroup/src/index.vue
+++ src/lib/d2-components/packages/D2ButtonGroup/src/index.vue
@@ -7,9 +7,17 @@
 <script>
 import { computed } from 'vue'
 import classNames from 'classnames'
+import { buttonProps } from '../../D2Button/src/index.vue'
+
+const {
+  color
+} = buttonProps
 
 export default {
   name: 'D2ButtonGroup',
+  props: {
+    color
+  },
   setup () {
     const buttonGroupClassNames = computed(() => classNames('d2-button-group'))
     
