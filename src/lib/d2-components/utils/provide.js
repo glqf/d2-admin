efebb2daa1913c48fab74a0e5d4b3b0c7dcf838f
FairyEver <1711467488@qq.com>
2021-06-08 10:19:48 +0800

    refactor: :construction: archive

--- src/lib/d2-components/utils/provide.js
+++ src/lib/d2-components/utils/provide.js
@@ -4,20 +4,31 @@ import { pascalCase } from './string.js'
 
 /**
  * Generate provide based on the component name
- * @param {string} componentName component name
+ * @param {string} componentName component name should be returned by the makecomponentname function
  * @returns function provide like vue provide
+ * @example
+ *          const name = makeComponentName('buttonGroup')
+ *          const provide = provideGenerator(name)
+ *          provide('size', computed(() => props.size))
+ *          => provide:D2ButtonGroupSize
  */
 export function provideGenerator (componentName) {
   /**
    * The value returned should be used for the first parameter of the vue provide method
    * @param {string} name provide name like 'name' or 'first-name'
-   * @returns string
+   * @param {value} value vue provide value
    */
   return function (name, value) {
-    return provide(componentName + pascalCase(name), value)
+    provide(componentName + pascalCase(name), value)
   }
 }
 
+/**
+ * 
+ * @param {string} componentName component name should be returned by the makecomponentname function
+ * @param {*} name 
+ * @returns 
+ */
 export function injectName (componentName, name) {
   return componentName + pascalCase(name)
 }
