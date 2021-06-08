810dbae984d52426968091c69b470f054321c145
FairyEver <1711467488@qq.com>
2021-06-08 09:19:09 +0800

    refactor: :construction: archive

--- src/lib/d2-components/utils/string.js
+++ src/lib/d2-components/utils/string.js
@@ -6,12 +6,24 @@
  */
 
 import { camelCase } from 'lodash'
+import { isString } from './is.js'
 
+/**
+ * Convert string to pascalcase
+ * @param {string} value string to be processed
+ * @returns string
+ */
 export function pascalCase (value) {
   return titleCase(camelCase(value))
 }
 
+/**
+ * Convert string to titleCase
+ * @param {string} value string to be processed
+ * @returns string
+ */
 export function titleCase (value) {
-  if (value.length < 2) return value
+  if (!isString(value)) return ''
+  if (value.length < 2) return value.toLocaleUpperCase()
   return value[0].toLocaleUpperCase() + value.slice(1)
 }
