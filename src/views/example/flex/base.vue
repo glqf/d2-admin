1b4528b5831fc9122dd359a3313db64db79395bd
FairyEver <1711467488@qq.com>
2021-06-10 14:40:50 +0800

    refactor: :construction: archive

--- src/views/example/flex/base.vue
+++ src/views/example/flex/base.vue
@@ -4,9 +4,9 @@
       <template v-for="m in flex.main" :key="m">
         <template v-for="c in flex.cross" :key="c">
           <template v-for="b in flex.box" :key="b">
-            <d2-flex dir="left" cross="center" class="mb-1">
+            <d2-flex dir="left" cross="center" class="mb-4 group">
               <d2-flex
-                class="w-24 h-24 p-1 bg-gray-100 hover:bg-indigo-100 group rounded"
+                class="w-36 h-36 p-1 bg-gray-100 rounded"
                 :dir="d"
                 :main="m"
                 :cross="c"
@@ -14,13 +14,13 @@
                 <div
                   v-for="n in 3"
                   :key="n"
-                  class="m-1 p-1 bg-indigo-500 group-hover:bg-indigo-600 text-white rounded text-center"/>
+                  class="m-1 p-1 bg-gray-200 group-hover:bg-indigo-500 text-white rounded text-center"/>
               </d2-flex>
-              <p class="ml-4 text-lg">
-                <span class="text-gray-400 mr-4">dir="<span class="text-indigo-500 mx-1">{{ d }}</span>"</span>
-                <span class="text-gray-400 mr-4">main="<span class="text-indigo-500 mx-1">{{ m }}</span>"</span>
-                <span class="text-gray-400 mr-4">cross="<span class="text-indigo-500 mx-1">{{ c }}</span>"</span>
-                <span class="text-gray-400 mr-4">box="<span class="text-indigo-500 mx-1">{{ b }}</span>"</span>
+              <p class="ml-4 text-lg bg-gray-100 group-hover:bg-indigo-500 text-gray-400 group-hover:text-indigo-300 px-4 py-1 rounded">
+                <span class="mr-4">dir="<span class="text-indigo-500 group-hover:text-white mx-1">{{ d }}</span>"</span>
+                <span class="mr-4">main="<span class="text-indigo-500 group-hover:text-white mx-1">{{ m }}</span>"</span>
+                <span class="mr-4">cross="<span class="text-indigo-500 group-hover:text-white mx-1">{{ c }}</span>"</span>
+                <span>box="<span class="text-indigo-500 group-hover:text-white mx-1">{{ b }}</span>"</span>
               </p>
             </d2-flex>
           </template>
