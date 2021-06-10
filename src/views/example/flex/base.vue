849321e5873a0cda8ab20d036b980c18219bd8e4
FairyEver <1711467488@qq.com>
2021-06-10 13:53:32 +0800

    refactor: :construction: archive

--- src/views/example/flex/base.vue
+++ src/views/example/flex/base.vue
@@ -1,17 +1,33 @@
 <template>
-  <the-section title="flex">
-    <d2-flex>
-      <div>inner</div>
-    </d2-flex>
-  </the-section>
+  <template v-for="dirValue in flex.dir" :key="dirValue">
+    <template v-for="mainValue in flex.main" :key="mainValue">
+      <template v-for="crossValue in flex.cross" :key="crossValue">
+        <the-section :title="`dir:${dirValue} main:${mainValue} cross:${crossValue}`">
+          <d2-flex :class="ground" :main="mainValue" :cross="crossValue" :dir="dirValue">
+            <div :class="point">1</div>
+            <div :class="point">2</div>
+            <div :class="point">3</div>
+          </d2-flex>
+        </the-section>
+      </template>
+    </template>
+  </template>
 </template>
 
 <script>
+import { flex } from 'd2-components/utils/const.js'
 import TheSection from '../components/the-section.vue'
 
 export default {
   components: {
     TheSection
+  },
+  setup () {
+    return {
+      flex,
+      ground: 'w-32 h-32 p-1 bg-gray-100 rounded',
+      point: 'd2-flex m-1 bg-indigo-500'
+    }
   }
 }
 </script>
