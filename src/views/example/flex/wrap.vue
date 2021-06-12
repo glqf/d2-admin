af7b1d869a551382ba23c7eafae3feb3a8c57160
FairyEver <1711467488@qq.com>
2021-06-12 11:23:31 +0800

    refactor: :construction: archive

--- src/views/example/flex/wrap.vue
+++ src/views/example/flex/wrap.vue
@@ -1,12 +1,17 @@
 <template>
+  <the-section title="default">
+    <d2-flex class="flex-example" cross="top">
+      <div class="flex-example__item flex-example__item--size" :class="`flex-example__item--${n}`" v-for="n in 6" :key="n"/>
+    </d2-flex>
+  </the-section>
   <the-section title="wrap">
-    <d2-flex class="flex-example" wrap>
-      <div class="flex-example__item" :class="`flex-example__item--${n}`" v-for="n in 3" :key="n"/>
+    <d2-flex class="flex-example" cross="top" wrap>
+      <div class="flex-example__item flex-example__item--size" :class="`flex-example__item--${n}`" v-for="n in 6" :key="n"/>
     </d2-flex>
   </the-section>
   <the-section title="wrap-r">
-    <d2-flex class="flex-example" wrap-r>
-      <div class="flex-example__item" :class="`flex-example__item--${n}`" v-for="n in 3" :key="n"/>
+    <d2-flex class="flex-example" cross="top" wrap-r>
+      <div class="flex-example__item flex-example__item--size" :class="`flex-example__item--${n}`" v-for="n in 6" :key="n"/>
     </d2-flex>
   </the-section>
 </template>
@@ -23,12 +28,21 @@ export default {
 
 <style lang="scss" scoped>
 .flex-example {
+  align-content: flex-start;
   @apply w-24 h-24 p-1 mb-4 mr-4 bg-gray-100 rounded;
   .flex-example__item {
     @apply p-1 m-1 rounded-sm;
     &.flex-example__item--1 { @apply bg-indigo-600; }
-    &.flex-example__item--2 { @apply bg-indigo-300; }
-    &.flex-example__item--3 { @apply bg-indigo-100; }
+    &.flex-example__item--2 { @apply bg-indigo-500; }
+    &.flex-example__item--3 { @apply bg-indigo-400; }
+    &.flex-example__item--4 { @apply bg-indigo-300; }
+    &.flex-example__item--5 { @apply bg-indigo-200; }
+    &.flex-example__item--6 { @apply bg-indigo-100; }
+    &.flex-example__item--size {
+      flex-grow: 0;
+      flex-shrink: 0;
+      @apply w-4 h-4;
+    }
   }
 }
 </style>
