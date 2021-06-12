3568dd8d32bfa83d4de3e14251f856d5fa78a605
FairyEver <1711467488@qq.com>
2021-06-12 20:26:18 +0800

    refactor: :construction: archive

--- src/views/example/flex/layout.vue
+++ src/views/example/flex/layout.vue
@@ -1,7 +1,9 @@
 <template>
   <the-section title="layout">
-    <d2-flex class="layout">
-      layout
+    <d2-flex class="layout" dir="top" box="justify">
+      <div class="layout__header">header</div>
+      <d2-flex class="layout__body" center>body</d2-flex>
+      <div class="layout__footer">footer</div>
     </d2-flex>
   </the-section>
 </template>
@@ -18,9 +20,15 @@ export default {
 
 <style lang="scss" scoped>
 .layout {
-  @apply w-96 h-64 bg-gray-100 rounded overflow-hidden;
-  .layout__header {}
-  .layout__footer {}
-  .layout__body {}
+  @apply w-96 h-64 p-1 bg-gray-100 rounded overflow-hidden;
+  .layout__header {
+    @apply text-center text-lg text-gray-500 py-2 bg-gray-300;
+  }
+  .layout__footer {
+    @apply text-center text-lg text-gray-500 py-2 bg-gray-200;
+  }
+  .layout__body {
+    @apply bg-gray-400;
+  }
 }
 </style>
