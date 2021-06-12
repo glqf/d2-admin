ceb9b32796f227f950e4c6926bacb72143db8fa8
FairyEver <1711467488@qq.com>
2021-06-12 19:58:12 +0800

    refactor: :construction: archive

---
+++ src/views/example/flex/layout.vue
@@ -0,0 +1,26 @@
+<template>
+  <the-section title="layout">
+    <d2-flex class="layout">
+      layout
+    </d2-flex>
+  </the-section>
+</template>
+
+<script>
+import TheSection from '../components/the-section.vue'
+
+export default {
+  components: {
+    TheSection
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.layout {
+  @apply w-96 h-64 bg-gray-100 rounded overflow-hidden;
+  .layout__header {}
+  .layout__footer {}
+  .layout__body {}
+}
+</style>
