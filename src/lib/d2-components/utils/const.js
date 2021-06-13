a5a83b4632ca573d2ba7e2a88b73963b352be09a
FairyEver <1711467488@qq.com>
2021-06-13 15:05:47 +0800

    refactor: :construction: archive

--- src/lib/d2-components/utils/const.js
+++ src/lib/d2-components/utils/const.js
@@ -2,69 +2,20 @@
 export const nameSpace = 'd2'
 
 // color names from tailwindcss basic color names
-export const colors = [
-  'gray',
-  'red',
-  'yellow',
-  'green',
-  'blue',
-  'indigo',
-  'purple',
-  'pink'
-]
+export const colors = ['gray', 'red', 'yellow', 'green', 'blue', 'indigo', 'purple', 'pink']
 
-export const sizes = [
-  'mini',
-  'small',
-  'large'
-]
+// space size
+// button size
+// button group size
+export const sizes = ['mini', 'small', 'large']
 
-export const buttonTypes = [
-  'button',
-  'submit',
-  'reset'
-]
+export const buttonTypes = ['button', 'submit', 'reset']
 
 export const flex = {
-  dir: [
-    'top',
-    'right',
-    'bottom',
-    'left'
-  ],
-  main: [
-    'right',
-    'left',
-    'justify',
-    'center'
-  ],
-  cross: [
-    'top',
-    'bottom',
-    'baseline',
-    'center',
-    'stretch'
-  ],
-  box: [
-    'mean',
-    'first',
-    'last',
-    'justify'
-  ],
-  content: [
-    'start',
-    'end',
-    'center',
-    'between',
-    'around',
-    'stretch'
-  ],
-  self: [
-    'auto',
-    'top',
-    'bottom',
-    'baseline',
-    'center',
-    'stretch'
-  ]
+  dir: ['top', 'right', 'bottom', 'left'],
+  main: ['right', 'left', 'justify', 'center'],
+  cross: ['top', 'bottom', 'baseline', 'center', 'stretch'],
+  box: ['mean', 'first', 'last', 'justify'],
+  content: ['start', 'end', 'center', 'between', 'around', 'stretch'],
+  self: ['auto', 'top', 'bottom', 'baseline', 'center', 'stretch']
 }
