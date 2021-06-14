0f26be58136b236ecfc29571d0151ab68c31c4a8
FairyEver <1711467488@qq.com>
2021-06-14 11:58:33 +0800

    refactor: :construction: archive

---
+++ src/views/example/break-point/base.vue
@@ -0,0 +1,15 @@
+<template>
+  <the-section title="break point">
+    123
+  </the-section>
+</template>
+
+<script>
+import TheSection from '../components/the-section.vue'
+
+export default {
+  components: {
+    TheSection
+  }
+}
+</script>
