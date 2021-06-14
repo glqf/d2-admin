3ff172da79fed6bb319c1d2934395cb36cdbdd13
FairyEver <1711467488@qq.com>
2021-06-14 10:12:43 +0800

    refactor: :construction: archive

--- src/views/example/icon/base.vue
+++ src/views/example/icon/base.vue
@@ -1,5 +1,11 @@
 <template>
-  <the-section title="切换数据">
+  
+  <the-section title="iconify">
+    <a href="https://iconify.design/icon-sets/">
+      <d2-button>Browse Icons</d2-button>
+    </a>
+  </the-section>
+  <the-section title="toggle">
     <d2-button @click="toggle">Toggle arrow</d2-button>
   </the-section>
   <the-section title="base">
@@ -7,44 +13,44 @@
     <d2-icon icon="mdi:arrow-left"/>
     <d2-icon :icon="`mdi:arrow-${ arrow }`"/>
   </the-section>
-  <the-section title="颜色 通过 class 设置">
+  <the-section title="color by class">
     <d2-icon class="text-indigo-500" icon="mdi:account-circle"/>
     <d2-icon class="text-indigo-500" icon="mdi:arrow-left"/>
     <d2-icon class="text-indigo-500" :icon="`mdi:arrow-${ arrow }`"/>
   </the-section>
-  <the-section title="颜色 通过 style 设置">
+  <the-section title="color by style">
     <d2-icon style="color: #6366F1;" icon="mdi:account-circle"/>
     <d2-icon style="color: #6366F1;" icon="mdi:arrow-left"/>
     <d2-icon style="color: #6366F1;" :icon="`mdi:arrow-${ arrow }`"/>
   </the-section>
-  <the-section title="大小 通过 class 设置">
+  <the-section title="size by class">
     <d2-icon class="text-4xl" icon="mdi:account-circle"/>
     <d2-icon class="text-4xl" icon="mdi:arrow-left"/>
     <d2-icon class="text-4xl" :icon="`mdi:arrow-${ arrow }`"/>
   </the-section>
-  <the-section title="大小 通过 style 设置">
+  <the-section title="size by style">
     <d2-icon style="font-size: 2.25rem; line-height: 2.5rem;" icon="mdi:account-circle"/>
     <d2-icon style="font-size: 2.25rem; line-height: 2.5rem;" icon="mdi:arrow-left"/>
     <d2-icon style="font-size: 2.25rem; line-height: 2.5rem;" :icon="`mdi:arrow-${ arrow }`"/>
   </the-section>
-  <the-section title="slot 传参">
+  <the-section title="slot">
     <d2-icon>mdi:account-circle</d2-icon>
     <d2-icon>mdi:arrow-left</d2-icon>
     <d2-icon>mdi:arrow-{{ arrow }}</d2-icon>
   </the-section>
-  <the-section title="设置图标类型">
+  <the-section title="icon collection">
     <d2-icon collection="mdi" icon="account-circle"/>
     <d2-icon collection="mdi" icon="arrow-left"/>
     <d2-icon collection="mdi" :icon="`arrow-${ arrow }`"/>
   </the-section>
-  <the-section title="设置图标类型 图标组">
+  <the-section title="icon collection icon group">
     <d2-icon-group collection="mdi">
       <d2-icon icon="account-circle"/>
       <d2-icon icon="arrow-left"/>
       <d2-icon :icon="`arrow-${ arrow }`"/>
     </d2-icon-group>
   </the-section>
-  <the-section title="图标组内单独设置">
+  <the-section title="cobertura icon group collection">
     <d2-icon-group collection="mdi">
       <d2-icon icon="account-circle"/>
       <d2-icon icon="arrow-left"/>
@@ -53,11 +59,11 @@
       <d2-icon collection="ri" icon="github-fill"/>
     </d2-icon-group>
   </the-section>
-  <the-section title="彩色图标">
+  <the-section title="color">
     <d2-icon icon="flat-color-icons:google"/>
     <d2-icon icon="flat-color-icons:steam"/>
   </the-section>
-  <the-section title="旋转">
+  <the-section title="spin">
     <d2-icon icon="mdi:loading" spin/>
   </the-section>
 </template>
