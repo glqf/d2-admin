4c78a3153cdc11d47d987499be137e361bcdbd9d
FairyEver <1711467488@qq.com>
2021-06-14 13:53:36 +0800

    refactor: :construction: archive

--- src/lib/d2-components/packages/break-point/src/break-point.jsx
+++ src/lib/d2-components/packages/break-point/src/break-point.jsx
@@ -10,14 +10,17 @@ const provide = provideGenerator(name)
 
 export default defineComponent({
   name,
+  props: {
+    config: { type: Object }
+  },
   setup (props, { slots }) {
     const $D2COMPONENT = useGlobalConfig()
 
-    const config = $D2COMPONENT.breakPoints
+    const config = props.config || $D2COMPONENT.breakPoints
 
     const { breakPoint } = useBreakPoint({ config })
 
-    provide('name', computed(() => 'hhh'))
+    provide('name', computed(() => breakPoint))
     
     return () => slots.default?.({
       breakPoint: unref(breakPoint)
