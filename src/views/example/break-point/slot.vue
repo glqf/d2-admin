84915445e38aca740bf2369e6ae4fea6d26af41e
FairyEver <1711467488@qq.com>
2021-06-15 11:06:07 +0800

    refactor: :construction: archive

--- src/views/example/break-point/slot.vue
+++ src/views/example/break-point/slot.vue
@@ -2,7 +2,7 @@
   <the-section title="break point">
     <d2-break-point v-slot="{ md }">
       <span v-if="md" class="text-indigo-500">this text only show when md</span>
-      <span v-else>resize window to md</span>
+      <span v-else class="text-gray-300">resize window to md</span>
     </d2-break-point>
   </the-section>
 </template>
