fc25303c4e66968ab0d820d6a0d0b541ac506033
FairyEver <1711467488@qq.com>
2021-06-15 10:40:14 +0800

    refactor: :construction: archive

--- src/views/example/use/break-point.vue
+++ src/views/example/use/break-point.vue
@@ -1,66 +1,20 @@
 <template>
-  <the-section title="break points">
-    <d2-button icon="gg:arrows-breake-h">
-      breakPoint {{ breakPoint }}
-    </d2-button>
-  </the-section>
-  <the-section title="data">
-    <div class="group">
-      <p :class="{ active: min }">
-        <span>min</span><span>{{ min }}</span>
-      </p>
-      <p :class="{ active: sm }">
-        <span>sm</span><span>{{ sm }}</span>
-      </p>
-      <p :class="{ active: md }">
-        <span>md</span><span>{{ md }}</span>
-      </p>
-      <p :class="{ active: lg }">
-        <span>lg</span><span>{{ lg }}</span>
-      </p>
-      <p :class="{ active: xl }">
-        <span>xl</span><span>{{ xl }}</span>
-      </p>
-      <p :class="{ active: xxl }">
-        <span>xxl</span><span>{{ xxl }}</span>
-      </p>
-    </div>
+  <the-section title="break point">
+    <d2-break-point v-slot="status">
+      <break-points-display :status="status"/>
+    </d2-break-point>
   </the-section>
 </template>
 
 <script>
 import { useBreakPoint } from 'd2-components/use/break-point.js'
 import TheSection from '../components/the-section.vue'
+import BreakPointsDisplay from '../components/break-points-display.vue'
 
 export default {
   components: {
-    TheSection
-  },
-  setup () {
-    const { breakPoint, sm, md, lg, xl, xxl, min } = useBreakPoint()
-
-    return { breakPoint, sm, md, lg, xl, xxl, min }
+    TheSection,
+    BreakPointsDisplay
   }
 }
 </script>
-
-<style lang="scss" scoped>
-.group {
-  p {
-    @apply mb-1 text-gray-300;
-    span {
-      &:first-child {
-        @apply inline-block w-8;
-      }
-    }
-    &.active {
-      @apply text-gray-500;
-      span {
-        &:last-child {
-          @apply text-indigo-500;
-        }
-      }
-    }
-  }
-}
-</style>
