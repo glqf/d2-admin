b17b52ca171446e48a2bf03854d59b9618b677e6
FairyEver <1711467488@qq.com>
2021-06-21 15:45:40 +0800

    refactor: :construction: archive

--- src/lib/d2-components/utils/is.js
+++ src/lib/d2-components/utils/is.js
@@ -5,7 +5,7 @@
  * It is not applicable to the use environment outside the project
  */
 
-import { isString, isNaN, isNumber } from 'lodash-es'
+import { isString } from 'lodash-es'
 import { colorNames, buttonSizeNames, spaceSizeNames, buttonTypes, flexProps } from './const.js'
 
 /**
@@ -60,17 +60,6 @@ export function isButtonTypes (value, empty) {
 }
 
 /**
- * Check if it can be converted to number
- * It is generally used for the verification of component props
- * Please set the data type on the component like 'type: [String, Number]'
- * @param {*} value value to check
- * @returns boolean
- */
-export function isNumberLike (value) {
-  return !isNaN(Number(value))
-}
-
-/**
  * Check if it is string and not empty
  * @param {*} value value to check
  * @returns boolean
@@ -86,35 +75,4 @@ export function isValuableString (value) {
  */
 export function isEmptyString (value) {
   return value === ''
-}
-
-/**
- * Check whether a value is an integer and less than the given value
- * @param {*} value value to check
- * @param {number} max max value
- * @returns boolean
- */
-export function isIntegerAndLessThan (value, max) {
-  return isNumber(value) && Number.isInteger(value) && value < max
-}
-
-/**
- * Check whether a value is an integer and greater than the given value
- * @param {*} value value to check
- * @param {number} min min value
- * @returns boolean
- */
-export function isIntegerAndGreaterThan (value, min) {
-  return isNumber(value) && Number.isInteger(value) && value > min
-}
-
-/**
- * Check that the value is an integer and within the given range
- * @param {*} value value to check
- * @param {number} min min value
- * @param {number} max max value
- * @returns boolean
- */
-export function isIntegerAndBetween (value, min, max) {
-  return isNumber(value) && Number.isInteger(value) && value >= min && value <= max
 }
