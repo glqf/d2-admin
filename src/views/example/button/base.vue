ba55394afe218950f053ee97d972e381f7f2dfd4
FairyEver <1711467488@qq.com>
2021-06-30 16:14:28 +0800

    refactor: :construction: archive

--- src/views/example/button/base.vue
+++ src/views/example/button/base.vue
@@ -5,6 +5,18 @@
   <the-section title="chinese">
     <d2-button>按钮</d2-button>
   </the-section>
+  <the-section title="inline">
+    <p class="align-middle">
+      text<d2-button>button</d2-button>text
+    </p>
+  </the-section>
+  <the-section title="inline el-button">
+    <p class="align-middle">
+      text
+      <button type="button" class="el-button">123</button>
+      text
+    </p>
+  </the-section>
   <the-section title="slot">
     <d2-button>
       <p class="text-lg text-left">Title</p>
@@ -22,3 +34,30 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.el-button {
+  display: inline-block;
+  line-height: 1;
+  white-space: nowrap;
+  cursor: pointer;
+  background: #FFF;
+  border: 1px solid #DCDFE6;
+  color: #606266;
+  -webkit-appearance: none;
+  text-align: center;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  outline: 0;
+  margin: 0;
+  -webkit-transition: .1s;
+  transition: .1s;
+  font-weight: 500;
+  padding: 12px 20px;
+  font-size: 14px;
+  border-radius: 4px;
+  color: #FFF;
+  background-color: #409EFF;
+  border-color: #409EFF;
+}
+</style>
