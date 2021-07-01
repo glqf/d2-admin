b5ed92fce9aa4f2cddf366b1149f13d29135b89f
FairyEver <1711467488@qq.com>
2021-07-01 16:58:51 +0800

    refactor: :construction: archive

--- src/views/example/button/base.vue
+++ src/views/example/button/base.vue
@@ -22,30 +22,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss" scoped>
-.el-button {
-  display: inline-block;
-  line-height: 1;
-  white-space: nowrap;
-  cursor: pointer;
-  background: #FFF;
-  border: 1px solid #DCDFE6;
-  color: #606266;
-  -webkit-appearance: none;
-  text-align: center;
-  -webkit-box-sizing: border-box;
-  box-sizing: border-box;
-  outline: 0;
-  margin: 0;
-  -webkit-transition: .1s;
-  transition: .1s;
-  font-weight: 500;
-  padding: 12px 20px;
-  font-size: 14px;
-  border-radius: 4px;
-  color: #FFF;
-  background-color: #409EFF;
-  border-color: #409EFF;
-}
-</style>
--- src/views/example/button/css.vue
+++ src/views/example/button/css.vue
@@ -1,3 +1,55 @@
 <template>
-  css
+  <the-section title="button">
+    <button class="css-button"><span>button</span></button>
+  </the-section>
+  <the-section title="button + icon">
+    <button class="css-button"><d2-icon icon="mdi:account-circle"/><span>button</span></button>
+  </the-section>
+  <the-section title="button + text">
+    <p>text<button class="css-button"><span>button</span></button>text</p>
+  </the-section>
+  <the-section title="button + icon + text">
+    <p>text<button class="css-button"><d2-icon icon="mdi:account-circle"/><span>button</span></button>text</p>
+  </the-section>
 </template>
+
+<script>
+import TheSection from '../components/the-section.vue'
+
+export default {
+  components: {
+    TheSection
+  }
+}
+</script>
+
+<style lang="scss">
+.css-button {
+  display: inline-block;
+  box-sizing: border-box;
+  background: #fff;
+  text-align: center;
+  margin: 0;
+  padding: 2px 4px;
+  font-size: 14px;
+  line-height: 1;
+  border: 1px solid #dcdfe6;
+  overflow: hidden;
+  vertical-align: text-bottom;
+  span {
+    display: inline-block;
+    background-color: #666666;
+    vertical-align: middle;
+  }
+  .d2-icon {
+    display: inline-block;
+    height: 1em;
+    line-height: 1;
+    background-color: #CCCCCC;
+    svg {
+      display: inline-block;
+      background-color: red;
+    }
+  }
+}
+</style>
