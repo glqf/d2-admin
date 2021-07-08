bdd0d35edf4699f93ba33e64e1fe23804a187353
FairyEver <1711467488@qq.com>
2021-07-08 17:45:30 +0800

    refactor: :construction: archive

--- src/views/example/button/loading.vue
+++ src/views/example/button/loading.vue
@@ -3,35 +3,35 @@
     <d2-button @click="toggle" :icon="loading ? 'mdi:toggle-switch' : 'mdi:toggle-switch-off-outline'" circle/>
   </the-section>
   <the-section title="loading">
-    <d2-button :loading="loading">button</d2-button>
+    <d2-button :loading="loading">Loading</d2-button>
   </the-section>
   <the-section title="right">
-    <d2-button :loading-right="loading">button</d2-button>
+    <d2-button :loading-right="loading">Loading</d2-button>
   </the-section>
   <the-section title="left icon is loading">
-    <d2-button icon="fluent:auto-fit-height-24-regular" :loading="loading">button</d2-button>
+    <d2-button icon="fluent:auto-fit-height-24-regular" :loading="loading">Loading</d2-button>
   </the-section>
   <the-section title="right icon is loading">
-    <d2-button icon-right="fluent:auto-fit-height-24-regular" :loading-right="loading">button</d2-button>
+    <d2-button icon-right="fluent:auto-fit-height-24-regular" :loading-right="loading">Loading</d2-button>
   </the-section>
   <the-section title="circle">
     <d2-button :loading="loading" icon="mdi:hand-right" circle/>
   </the-section>
   <the-section title="disabled">
-    <d2-button :loading="loading" disabled>button</d2-button>
+    <d2-button :loading="loading" disabled>Loading</d2-button>
   </the-section>
   <the-section title="plain">
-    <d2-button :loading="loading" plain>button</d2-button>
+    <d2-button :loading="loading" plain>Loading</d2-button>
   </the-section>
   <the-section title="round">
-    <d2-button :loading="loading" round>button</d2-button>
+    <d2-button :loading="loading" round>Loading</d2-button>
   </the-section>
   <the-section title="round half icon">
     <d2-button icon="akar-icons:chevron-left" :loading="loading" round-left>Left</d2-button>
     <d2-button icon-right="akar-icons:chevron-right" :loading-right="loading" round-right>Right</d2-button>
   </the-section>
   <the-section title="text">
-    <d2-button :loading="loading" text>button</d2-button>
+    <d2-button :loading="loading" text>Loading</d2-button>
   </the-section>
 </template>
 
