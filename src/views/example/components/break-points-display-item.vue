60f20e9d75bce07e2869a92a55fa4374b4436e98
FairyEver <1711467488@qq.com>
2021-07-09 14:29:18 +0800

    refactor: :construction: archive

---
+++ src/views/example/components/break-points-display-item.vue
@@ -0,0 +1,31 @@
+<template>
+  <d2-flex dir="top" cross="center" class="item">
+    <d2-icon v-if="value" class="item__icon item__icon--active" icon="akar-icons:circle-check-fill"/>
+    <d2-icon v-else class="item__icon" icon="akar-icons:circle-x"/>
+    <span class="item__label">{{ name }}</span>
+  </d2-flex>
+</template>
+
+<script>
+export default {
+  props: {
+    value: { type: Boolean },
+    name: { type: String }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.item {
+  @apply w-14;
+  .item__label {
+    @apply text-lg text-gray-500;
+  }
+  .item__icon {
+    @apply text-2xl text-gray-300;
+    &.item__icon--active {
+      @apply text-green-500;
+    }
+  }
+}
+</style>
--- src/views/example/components/break-points-display.vue
+++ src/views/example/components/break-points-display.vue
@@ -1,23 +1,27 @@
 <template>
   <h1 class="break-point-display__title">{{ status.breakPoint }}</h1>
   <d2-flex class="break-point-display__data" center>
-    <d2-flex dir="top" cross="center" class="data__item">
-      <d2-icon v-if="status.min" class="item__icon item__icon--active" icon="akar-icons:circle-check-fill"/>
-      <d2-icon v-else class="item__icon" icon="akar-icons:circle-x"/>
-      <span class="item__label">min</span>
-    </d2-flex>
-    <d2-flex v-for="name in breakPointsName" :key="name" dir="top" cross="center" class="data__item">
-      <d2-icon v-if="status[name]" class="item__icon item__icon--active" icon="akar-icons:circle-check-fill"/>
-      <d2-icon v-else class="item__icon" icon="akar-icons:circle-x"/>
-      <span class="item__label">{{ name }}</span>
-    </d2-flex>
+    <break-points-display-item
+      :value="status.min"
+      name="min"
+    />
+    <break-points-display-item
+      v-for="name in breakPointsName"
+      :key="name"
+      :value="status[name]"
+      :name="name"
+    />
   </d2-flex>
 </template>
 
 <script>
 import { breakPointsName } from 'd2-components/utils/const.js'
+import BreakPointsDisplayItem from './break-points-display-item.vue'
 
 export default {
+  components: {
+    BreakPointsDisplayItem
+  },
   props: {
     status: { type: Object }
   },
@@ -33,19 +37,4 @@ export default {
 .break-point-display__title {
   @apply mb-4 text-center text-gray-500 text-9xl;
 }
-.break-point-display__data {
-  .data__item {
-    @apply w-14;
-    .item__label {
-      @apply text-lg text-gray-500;
-    }
-    .item__icon {
-      // @apply text-2xl text-gray-300;
-      &.item__icon--active {
-        @apply text-green-500;
-      }
-    }
-  }
-}
 </style>
-
