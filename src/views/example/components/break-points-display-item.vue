c950d10ce31e85ced90845eb66e5ba43cab659cf
FairyEver <1711467488@qq.com>
2021-07-09 16:29:09 +0800

    refactor: :construction: archive

--- src/views/example/components/break-points-display-item.vue
+++ src/views/example/components/break-points-display-item.vue
@@ -27,7 +27,19 @@ export default {
 
 <style lang="scss" scoped>
 .item {
-  @apply w-14;
+  @apply p-2 rounded cursor-pointer select-none w-14;
+  &:hover {
+    @apply bg-green-500;
+    .item__label {
+      @apply text-white;
+    }
+    .item__icon {
+      @apply text-white;
+      &.item__icon--active {
+        @apply text-white;
+      }
+    }
+  }
   .item__label {
     @apply text-lg text-gray-500;
   }
