273f004586ec0b8e08e98a8dfdbb196d676ca10b
FairyEver <1711467488@qq.com>
2021-07-09 22:29:06 +0800

    refactor: :construction: archive

--- src/lib/d2-components/packages/input/src/input.jsx
+++ src/lib/d2-components/packages/input/src/input.jsx
@@ -1,4 +1,4 @@
-import { defineComponent, computed } from 'vue'
+import { defineComponent, computed, ref, watch } from 'vue'
 import classNames from 'classnames'
 import { makeComponentName, makeComponentClassName } from '../../../utils/make.js'
 
@@ -7,14 +7,38 @@ export const mainClassName = makeComponentClassName('input')
 
 export default defineComponent({
   name,
-  props: {},
-  setup (props, { slots }) {
+  props: {
+    value: { type: [String, Number], default: '' }
+  },
+  emits: [
+    'update:value'
+  ],
+  setup (props, { slots, emit }) {
+    const currentValue = ref(props.value || '')
+
+    watch(() => props.value, (value) => {
+      currentValue.value = value
+    })
+
     const inputClassName = computed(() => classNames(
       mainClassName,
       {}
     ))
+
+    function handleChange (e) {
+      const value = e.target.value
+      currentValue.value = value
+      emit('update:value', value)
+    }
     
-    return () =>
-      <input class={ inputClassName.value }></input>
+    return () => <div>
+      <p>{ currentValue.value }</p>
+      <input
+        class={ inputClassName.value }
+        value={ currentValue.value }
+        onInput={ handleChange }
+        onChange={ handleChange }
+      />
+    </div>
   }
 })
--- src/views/example/input/base.vue
+++ src/views/example/input/base.vue
@@ -1,15 +1,25 @@
 <template>
   <the-section title="input">
-    <d2-input></d2-input>
+    <p class="mb-4">{{ value }}</p>
+    <d2-input v-model:value="value"/>
+    <d2-input v-model:value="value"/>
   </the-section>
 </template>
 
 <script>
+import { ref } from 'vue'
 import TheSection from '../components/the-section.vue'
 
 export default {
   components: {
     TheSection
+  },
+  setup () {
+    const value = ref('Hello World')
+
+    return {
+      value
+    }
   }
 }
 </script>
