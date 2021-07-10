3281b997af10b4b6334289558a4c6853d2c0d9f9
FairyEver <1711467488@qq.com>
2021-07-10 20:24:13 +0800

    refactor: :construction: archive

---
+++ src/views/example/input/color.vue
@@ -0,0 +1,26 @@
+<template>
+  <the-section title="color">
+    <d2-input v-model:value="value" class="mb-4"/>
+    <d2-input v-for="color in colorNames" :key="color" :color="color" v-model:value="value" class="mb-4"/>
+  </the-section>
+</template>
+
+<script>
+import { ref } from 'vue'
+import { colorNames } from 'd2-components/utils/const.js'
+import TheSection from '../components/the-section.vue'
+
+export default {
+  components: {
+    TheSection
+  },
+  setup () {
+    const value = ref('Hello World')
+
+    return {
+      value,
+      colorNames
+    }
+  }
+}
+</script>
