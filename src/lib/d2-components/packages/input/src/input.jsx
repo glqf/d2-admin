d9acbf12972ce1dd87aa57979ce06dff3e7c6780
FairyEver <1711467488@qq.com>
2021-07-13 08:49:54 +0800

    refactor: :construction: archive

--- src/lib/d2-components/packages/input/src/input.jsx
+++ src/lib/d2-components/packages/input/src/input.jsx
@@ -20,63 +20,56 @@ export default defineComponent({
   emits: [
     'update:value'
   ],
-  setup (props, { attrs, slots, emit }) {
+  setup (props, context) {
     const $D2COM = useConfigForD2Components()
 
-    const currentValue = ref(props.value || '')
-
-    watch(() => props.value, (value) => {
-      currentValue.value = value
-    })
-
-    function handleChange (e) {
-      const value = e.target.value
-      currentValue.value = value
-      emit('update:value', value)
-    }
-
     function createInputElement ({
-      classNames = '',
+      classInner = '',
       disabled
     }) {
-      const props = {
-        disabled,
-        class: classNames,
-        value: currentValue.value,
-        onInput: handleChange,
-        onChange: handleChange
+      const { emit } = context
+      const currentValue = ref(props.value || '')
+      watch(() => props.value, (value) => {
+        currentValue.value = value
+      })
+      function onInputElementChange (e) {
+        const value = e.target.value
+        currentValue.value = value
+        emit('update:value', value)
       }
-      return <input { ...props }/>
+      const inputElementProps = {
+        disabled,
+        class: classInner,
+        value: currentValue.value,
+        onInput: onInputElementChange,
+        onChange: onInputElementChange
+      }
+      return <input { ...inputElementProps }/>
     }
 
     function createInputComponent () {
       const inputDisabled = computed(() => props.disabled)
-  
       const inputSize = computed(() => props.size || $D2COM.size)
-  
       const inputColor = computed(() => props.color)
-
       const wrapperActive = computed(() => false)
-
-      const inputElementClassName = computed(() => classNames(
+      const classInner = computed(() => classNames(
         mainClassName,
         {
           'is-disabled': inputDisabled.value,
           [`${mainClassName}--${inputSize.value}`]: inputSize.value,
           [`${mainClassName}--${inputColor.value}`]: inputColor.value,
-          [attrs.class]: attrs.class && !wrapperActive.value
+          [context.attrs.class]: context.attrs.class && !wrapperActive.value
         }
       ))
-
       return () => {
         const input = createInputElement({
-          classNames: inputElementClassName.value,
+          classInner: classInner.value,
           disabled: inputDisabled.value
         })
         return input
       }
     }
     
-    return createInputComponent()
+    return createInputComponent(props, context)
   }
 })
