4ddad7893a3b2c3bffaa974118017e9544d59b5b
FairyEver <1711467488@qq.com>
2021-07-21 14:42:55 +0800

    refactor: :construction: archive

---
+++ src/views/example/button/type.vue
@@ -0,0 +1,22 @@
+<template>
+  <the-section title="type">
+    <d2-button>Apple</d2-button>
+    <d2-button v-for="type in buttonTypes" :key="type" :type="type">Apple</d2-button>
+  </the-section>
+</template>
+
+<script>
+import { buttonTypes } from 'd2-components/utils/const.js'
+import TheSection from '../components/the-section.vue'
+
+export default {
+  components: {
+    TheSection
+  },
+  setup () {
+    return {
+      buttonTypes
+    }
+  }
+}
+</script>
