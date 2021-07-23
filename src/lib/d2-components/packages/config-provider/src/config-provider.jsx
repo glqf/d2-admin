defc406e2fa4c293effdc84c67f127a7af23a0a5
FairyEver <1711467488@qq.com>
2021-07-23 15:55:36 +0800

    refactor: :construction: archive

--- src/lib/d2-components/packages/config-provider/src/config-provider.jsx
+++ src/lib/d2-components/packages/config-provider/src/config-provider.jsx
@@ -5,7 +5,7 @@ export const name = makeComponentName('config-provider')
 
 export default defineComponent({
   name,
-  setup () {
-    return () => <div>config-provider</div>
+  setup (props, { slots }) {
+    return () => <div>{ slots.default() }</div>
   }
 })
