24006851f1ead33c675d01cfcd89f71eb5da089e
FairyEver <1711467488@qq.com>
2021-07-26 16:19:44 +0800

    refactor: :construction: archive

--- src/lib/d2-components/packages/button/src/button.jsx
+++ src/lib/d2-components/packages/button/src/button.jsx
@@ -88,7 +88,10 @@ export default defineComponent({
         )
     
     function checkContentIsTwoCNChar () {
-      console.log(buttonRef.value.textContent)
+      const textContent = buttonRef.value.textContent
+      if (isTwoCNChar(textContent)) {
+        console.log('isTwoCNChar', textContent)
+      }
     }
 
     onMounted(checkContentIsTwoCNChar)
