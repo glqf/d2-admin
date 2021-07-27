15d384db5594afb49b8a691935f81118de8403d8
FairyEver <1711467488@qq.com>
2021-07-27 09:33:31 +0800

    refactor: :construction: archive

--- src/lib/d2-components/packages/config-provider/src/config-provider.jsx
+++ src/lib/d2-components/packages/config-provider/src/config-provider.jsx
@@ -1,11 +1,38 @@
-import { defineComponent } from 'vue'
+import { defineComponent, provide, reactive } from 'vue'
 import { makeComponentName } from '../../../utils/make.js'
+import { breakPoints } from '../../../utils/const.js'
 
 export const name = makeComponentName('config-provider')
 
+const configDefault = {
+  svgPrefix: '',
+  svgNames: [],
+  size: '',
+  iconCollection: '',
+  windowResizeThrottleWait: 100,
+  breakPoints
+}
+
+console.log(JSON.stringify(configDefault, null, 2))
+
 export default defineComponent({
   name,
+  props: {
+
+  },
   setup (props, { slots }) {
-    return () => <div>{ slots.default() }</div>
+    const data = reactive({
+      ...props
+    })
+    Object.keys(props).forEach(key => {
+      watch(
+        () => props[key],
+        () => {
+          data[key] = props[key]
+        }
+      )
+    })
+    provide(name, data)
+    return () => slots.default && slots.default()
   }
 })
