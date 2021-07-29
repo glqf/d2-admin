2e0f460c2aee0daf8fe51f8b84a4f0dc073631c8
FairyEver <1711467488@qq.com>
2021-07-29 09:21:04 +0800

    refactor: :construction: archive

--- src/lib/d2-components/packages/button/src/button-group.jsx
+++ src/lib/d2-components/packages/button/src/button-group.jsx
@@ -1,7 +1,8 @@
 import { defineComponent, computed, provide, reactive, watch } from 'vue'
 import classNames from 'classnames'
-import { fromPairs, pick } from 'lodash-es'
+import { pick } from 'lodash-es'
 import { useConfig } from '../../../use/config.js'
+import { provideData } from '../../../use/provide.js'
 import { makeComponentName, makeComponentClassName } from '../../../utils/make.js'
 import buttonProps from './props.js'
 
@@ -16,18 +17,7 @@ export default defineComponent({
   setup (props, { slots }) {
     const config = useConfig()
 
-    const provideData = reactive(fromPairs(propsNames.map(key => [key, props[key]])))
-
-    provide(name, provideData)
-
-    propsNames.forEach(key => {
-      watch(
-        () => props[key],
-        () => {
-          provideData[key] = props[key]
-        }
-      )
-    })
+    provideData(name, props)
 
     const buttonGroupSize = computed(() => props.size || config.size)
 
