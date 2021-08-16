e4aae8e8f2a6e3548057017f912359ba84448da1
FairyEver <1711467488@qq.com>
2021-08-16 12:28:21 +0800

    feat: add ant-design-vue

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,3 +1,3 @@
 <template>
-  index
+  <a-button>Index</a-button>
 </template>
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 
+// https://github.com/antfu/vite-plugin-components
+import ViteComponents, { AntDesignVueResolver } from 'vite-plugin-components'
+
 // https://github.com/hannoeru/vite-plugin-pages
 import Pages from 'vite-plugin-pages'
 
 export default defineConfig({
   plugins: [
     Vue(),
+    ViteComponents({
+      customComponentResolvers: [
+        AntDesignVueResolver()
+      ]
+    }),
     Pages({
       pagesDir: 'src/views',
       exclude: [
