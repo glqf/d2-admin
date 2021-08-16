9b4b64ede46b81632245bf71d70efa5bf2936e1a
FairyEver <1711467488@qq.com>
2021-08-16 13:16:04 +0800

    refactor: :construction: archive

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 
+import { visualizer } from 'rollup-plugin-visualizer'
+
+// https://github.com/nekocode/antd-dayjs-vite-plugin
+import AntdDayjs from 'antd-dayjs-vite-plugin'
+
 // https://github.com/antfu/vite-plugin-components
-import ViteComponents, { AntDesignVueResolver } from 'vite-plugin-components'
+import Components, { AntDesignVueResolver } from 'vite-plugin-components'
 
 // https://github.com/hannoeru/vite-plugin-pages
 import Pages from 'vite-plugin-pages'
@@ -10,7 +15,7 @@ import Pages from 'vite-plugin-pages'
 export default defineConfig({
   plugins: [
     Vue(),
-    ViteComponents({
+    Components({
       customComponentResolvers: [
         AntDesignVueResolver()
       ]
@@ -21,6 +26,12 @@ export default defineConfig({
         '**/components/*.vue'
       ],
       extensions: ['vue', 'jsx']
+    }),
+    AntdDayjs({
+      preset: 'antdv3'
+    }),
+    visualizer({
+      open: true
     })
   ],
   resolve: {
