f4facade6959a9e63cea1fad4b5e9d59a53a5d05
FairyEver <1711467488@qq.com>
2021-08-18 09:51:11 +0800

    feat: launch.json

--- src/libs/d2-components/packages/config/src/config.jsx
+++ src/libs/d2-components/packages/config/src/config.jsx
@@ -10,32 +10,39 @@ const componentProps = {
   testKey: { type: String, default: '' }
 }
 
-function getPropDefault (key) {
+function propDefault (key) {
   const defaultConfig = componentProps?.[key]?.default
   if (isFunction(defaultConfig)) return defaultConfig()
   return defaultConfig
 }
 
 function validProp (key, ...values) {
-  const defaultValue = getPropDefault(key)
+  const defaultValue = propDefault(key)
   return values.find(e => !isEqual(defaultValue, e))
 }
 
 export function useConfig () {
   const config = inject(provideKey, provideDataDefault)
-  const result = fromPairs(keys(componentProps).map(key => [
-    key,
-    computed(() => config[key])
-  ]))
+  const result = fromPairs(
+    keys(componentProps).map(key => [
+      key,
+      computed(() => config[key])
+    ])
+  )
   return result
 }
 
 export default defineComponent({
   name: 'D2ComponentsConfig',
   props: componentProps,
-  setup (props, { slots, attrs }) {
+  setup (props, { slots }) {
     const config = useConfig()
-    const provideData = reactive(mapValues(props, (value, key) => validProp(key, value, config[key].value)))
+    const provideData = reactive(
+      mapValues(
+        props,
+        (value, key) => validProp(key, value, config[key].value)
+      )
+    )
     keys(props).forEach(key => {
       watch(
         () => props[key],
