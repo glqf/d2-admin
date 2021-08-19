4858261c4d507eb8f8d34004562c34cff3f44f5e
FairyEver <1711467488@qq.com>
2021-08-19 11:00:47 +0800

    feat: break-points demo

--- src/views/use/break-points.vue
+++ src/views/use/break-points.vue
@@ -1,23 +1,17 @@
 <template>
-  <p>active: {{ active }}</p>
-  <p>min: {{ min }}</p>
-  <p>sm: {{ sm }}</p>
-  <p>md: {{ md }}</p>
-  <p>lg: {{ lg }}</p>
-  <p>xl: {{ xl }}</p>
-  <p>xxl: {{ xxl }}</p>
+  <pre>{{ status }}</pre>
+  <p>width: {{ width }}</p>
 </template>
 
 <script setup>
 import { useBreakPoint } from 'd2-use/break-point.js'
 
-const {
-  active,
-  min,
-  sm,
-  md,
-  lg,
-  xl,
-  xxl
-} = useBreakPoint()
+const status = useBreakPoint()
+
+const responsive = status.responsive
+
+const width = responsive(100, {
+  md: 50,
+  lg: 25
+})
 </script>
