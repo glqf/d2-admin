7e11454bfe1e4a44f518a8b7d29d0d6e27a962ac
FairyEver <1711467488@qq.com>
2021-08-24 20:28:45 +0800

    refactor: :construction: archive

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import {
 import App from './App.vue'
 import router from '@/router.js'
 
-import D2Admin from './plugins/d2-admin/d2-admin.js'
+import D2Admin from './plugins/d2-admin.js'
 
 import 'virtual:svg-icons-register'
 import '@purge-icons/generated'
