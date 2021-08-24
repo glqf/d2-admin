0dc5726e7de884d6868bca4258a48d0f9f180f5f
FairyEver <1711467488@qq.com>
2021-08-24 08:38:25 +0800

    refactor: :construction: archive

--- d2-projects/d2-use/config.js
+++ d2-projects/d2-use/config.js
@@ -1,7 +1,5 @@
 import { inject } from 'vue'
 
-export const provideName = '__D2_COMPONENTS_CONFIG__'
-
 export function useD2ComponentsConfig () {
-  return inject(provideName, {})
+  return inject('__D2_COMPONENTS_CONFIG__', {})
 }
