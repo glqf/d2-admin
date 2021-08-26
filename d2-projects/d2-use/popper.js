463cca46ddd2ebe553aa573a48d9d1b809daee77
FairyEver <1711467488@qq.com>
2021-08-26 14:14:49 +0800

    refactor: :construction: archive

--- d2-projects/d2-use/popper.js
+++ d2-projects/d2-use/popper.js
@@ -4,19 +4,21 @@ import { $, findElement } from 'd2-projects/d2-utils/vue.js'
 import { usePopperOptions } from './popper-options.js'
 
 export function usePopper (props) {
-  const instance = null
+  let instance = null
+
   const refTrigger = $(null)
   const refPopper = $(null)
-  const popperOptions = usePopperOptions(props)
+  
+  const optionsComputed = usePopperOptions(props)
 
   function init () {
     const _trigger = findElement($(refTrigger))
     const _popper = $(refPopper)
-    const _options = $(popperOptions)
+    const _options = $(optionsComputed)
     instance = createPopper(_trigger, _popper, _options)
   }
 
-  watch(popperOptions, options => {
+  watch(optionsComputed, options => {
     if (!instance) return
     setOptions(options)
     update()
