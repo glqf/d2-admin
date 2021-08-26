063bf8727bb359fcc11ecaa84d8cab7a55925f09
FairyEver <1711467488@qq.com>
2021-08-26 17:16:17 +0800

    refactor: :construction: archive

--- d2-projects/d2-use/popper-options.js
+++
@@ -1,7 +0,0 @@
-import { $ } from 'd2-projects/d2-utils/vue.js'
-
-export function usePopperOptions () {
-  return $(() => ({
-    placement: 'bottom'
-  }))
-}
--- d2-projects/d2-use/popper.js
+++ d2-projects/d2-use/popper.js
@@ -1,31 +1,125 @@
-import { onBeforeUpdate, watch, watchPostEffect } from 'vue'
-import { $ } from 'd2-projects/d2-utils/vue.js'
-import { usePopperInstance } from './popper-instance.js'
-import { usePopperVisible } from './popper-visible.js'
+import { computed, onBeforeUpdate, watch, watchPostEffect } from 'vue'
+import { isBoolean } from 'lodash-es'
+import { createPopper } from '@popperjs/core'
+import { $, findElement } from 'd2-projects/d2-utils/vue.js'
 
 /**
  * @param {boolean} props.visible
  * @param {boolean} props.disabled
  * @param {boolean} props.manualMode
+ * @param {number} props.autoClose
+ * @param {number} props.showAfter
+ * @param {number} props.hideAfter
  * @param {string} props.trigger click | focus | hover | manual
  */
 export function usePopper (props) {
-  const {
-    instance,
-    refTrigger,
-    refPopper,
-    optionsComputed,
-    destroy,
-    update,
-    forceUpdate,
-    setOptions,
-    init
-  } = usePopperInstance(props)
+  let instance = null
 
-  const {
-    visible,
-    visibleComputed
-  } = usePopperVisible(props)
+  const refTrigger = $(null)
+  const refPopper = $(null)
+
+  const optionsComputed = $(() => ({
+    placement: 'bottom'
+  }))
+
+  const isManualMode = $(() => props.manualMode || props.trigger === 'manual')
+
+  const hasVisibleProp = $(() => isBoolean(props.visible))
+  const visible = $(!!props.visible)
+
+  let showTimer = null
+  let hideTimer = null
+
+  const visibleComputed = computed({
+    get () {
+      return props.disabled
+        ? false
+        : (
+            $(hasVisibleProp)
+              ? props.visible
+              : $(visible)
+          )
+    },
+    set (val) {
+      if ($(isManualMode)) return
+      $(hasVisibleProp)
+        ? emit('update:visible', val)
+        : $(visible, val)
+    },
+  })
+
+  const instanceMethod = name => instance[name] || (() => {})
+
+  const destroy = () => instanceMethod('destroy')()
+  const update = () => instanceMethod('update')()
+  const forceUpdate = () => instanceMethod('forceUpdate')()
+  const setOptions = options => instanceMethod('setOptions')(options)
+
+  function init () {
+    const _trigger = findElement($(refTrigger))
+    const _popper = $(refPopper)
+    const _options = $(optionsComputed)
+    instance = createPopper(_trigger, _popper, _options)
+  }
+
+  function _show() {
+    if (props.autoClose > 0) {
+      hideTimer = setTimeout(() => {
+        _hide()
+      }, props.autoClose)
+    }
+    visibleComputed.value = true
+  }
+
+  function _hide() {
+    visibleComputed.value = false
+  }
+
+  function clearTimers() {
+    clearTimeout(showTimer)
+    clearTimeout(hideTimer)
+  }
+
+  const show = () => {
+    if ($(isManualMode) || props.disabled) return
+    clearTimers()
+    if (props.showAfter === 0) {
+      _show()
+    } else {
+      showTimer = setTimeout(() => {
+        _show()
+      }, props.showAfter)
+    }
+  }
+
+  const hide = () => {
+    if ($(isManualMode)) return
+    clearTimers()
+    if (props.hideAfter > 0) {
+      hideTimer = setTimeout(() => {
+        close()
+      }, props.hideAfter)
+    } else {
+      close()
+    }
+  }
+
+  const close = () => {
+    _hide()
+    if (props.disabled) {
+      doDestroy(true)
+    }
+  }
+
+  function doDestroy(force) {
+    if (!instance || ($(visibleComputed) && !force)) return
+    detachPopper()
+  }
+
+  function detachPopper() {
+    destroy()
+    instance = null
+  }
 
   watch(optionsComputed, options => {
     setOptions(options)
