4e114e248648a87ec57776a51dc990c92f73dd74
FairyEver <1711467488@qq.com>
2021-08-27 14:45:43 +0800

    feat: popper extraProps

--- d2-projects/d2-popper/src/render.jsx
+++ d2-projects/d2-popper/src/render.jsx
@@ -1,12 +1,12 @@
 import { Transition, createVNode, cloneVNode, withDirectives, withCtx, vShow } from 'vue'
 import { getFirstValidNode } from 'd2-projects/d2-utils/vnode.js'
 
-export function renderTrigger (trigger, props = {}) {
+export function renderTrigger (trigger, extraProps = {}) {
   const element = getFirstValidNode(trigger, 1)
   if (!element) {
     console.log('trigger expects single rooted node')
   }
-  return cloneVNode(element, props, true)
+  return cloneVNode(element, extraProps, true)
 }
 
 export function renderPopper (children, config = {}) {
