79b389feaaf8a236a87d5009d8e8de3de83595f8
FairyEver <1711467488@qq.com>
2021-08-30 20:50:29 +0800

    feat: use-popper

--- d2-projects/d2-popper/src/popper.jsx
+++ d2-projects/d2-popper/src/popper.jsx
@@ -1,6 +1,7 @@
 import makeClassnames from 'classnames'
-import { defineComponent, Teleport } from 'vue'
+import { defineComponent, Teleport, onMounted, onBeforeUnmount, onActivated, onDeactivated } from 'vue'
 import { $ } from 'd2-projects/d2-utils/vue.js'
+import { throwError } from 'd2-projects/d2-utils/error.js'
 import { usePopper, popperEmits } from 'd2-projects/d2-use/use-popper/index.js'
 import { popperPropsDefault } from 'd2-projects/d2-use/use-popper/props-default.js'
 import { makeComponentName, makeComponentClassName } from 'd2-projects/d2-utils/special/d2-components/name.js'
@@ -17,14 +18,24 @@ export default defineComponent({
   emits: [
     ...popperEmits
   ],
-  setup (props, { emit }) {
-    const popperCtx = usePopper(props, emit)
+  setup (props, { slots, emit }) {
+    if (slots.trigger) {
+      throwError(componentName, 'Trigger must be provided')
+    }
+
+    const popperStates = usePopper(props, emit)
+
+    const forceDestroy = () => popperStates.doDestroy(true)
+    onMounted(popperStates.initializePopper)
+    onBeforeUnmount(forceDestroy)
+    onActivated(popperStates.initializePopper)
+    onDeactivated(forceDestroy)
 
     const classnames = $(() => makeClassnames(classname, {}))
 
     return {
       classnames,
-      ...popperCtx
+      ...popperStates
     }
   },
   render () {
