3211141498754e44bf968c79e323197819666f6a
FairyEver <1711467488@qq.com>
2021-09-01 10:35:07 +0800

    docs: doc

--- vite.config.js
+++ vite.config.js
@@ -9,9 +9,6 @@ import { visualizer } from 'rollup-plugin-visualizer'
 // https://github.com/antfu/vite-plugin-md
 import Markdown from 'vite-plugin-md'
 
-// https://github.com/visualfanatic/vite-svg/tree/vite-plugin-svg
-import Svg from 'vite-plugin-vue-svg'
-
 // https://github.com/vitejs/vite/tree/main/packages/plugin-vue-jsx
 import Jsx from '@vitejs/plugin-vue-jsx'
 
@@ -25,15 +22,20 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://github.com/hannoeru/vite-plugin-pages
 import Pages from 'vite-plugin-pages'
 
+// https://github.com/visualfanatic/vite-svg/tree/vite-plugin-svg
+// support: load svg image as vue component
+import Svg from 'vite-plugin-vue-svg'
+
 // https://github.com/anncwb/vite-plugin-svg-icons
+// support: load all SVG images from the specified folder as icons
 import SvgIcons from 'vite-plugin-svg-icons'
 
 // https://github.com/antfu/purge-icons
-// for support: <span class="iconify" data-icon="fa:home"></span>
+// support: <span class="iconify" data-icon="fa:home"></span>
 import PurgeIcons from 'vite-plugin-purge-icons'
 
 // https://github.com/antfu/unplugin-icons
-// for support: <i-mdi-account-box/>
+// support: <i-mdi-account-box/>
 import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
 
