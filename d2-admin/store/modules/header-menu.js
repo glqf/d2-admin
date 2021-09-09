2d7f9ad272705832acf07582be78e06f12ea6785
FairyEver <1711467488@qq.com>
2021-09-09 16:13:40 +0800

    refactor: header-menu keyPrefix

--- d2-admin/store/modules/header-menu.js
+++ d2-admin/store/modules/header-menu.js
@@ -1,4 +1,4 @@
-import { camelCase, mapKeys } from 'lodash-es'
+import { keyPrefix } from 'd2-projects/d2-utils/object.js'
 import { menuStore } from './menu.js'
 
-export const headerMenuStore = mapKeys(menuStore(), (_, key) => camelCase(`header-${key}`))
+export const asideMenuStore = keyPrefix(menuStore(), 'header')
