e3d7105c2be5c48fcdaa5b31698de4aa2fc44712
FairyEver <1711467488@qq.com>
2021-09-09 16:27:51 +0800

    fix: hook

--- d2-admin/store/modules/aside-menu.js
+++ d2-admin/store/modules/aside-menu.js
@@ -1,4 +1,4 @@
 import { keyPrefix } from 'd2-projects/d2-utils/object.js'
 import { menuStore } from './menu.js'
 
-export const asideMenuStore = keyPrefix(menuStore(), 'aside')
+export const asideMenuStore = () => keyPrefix(menuStore(), 'aside')
--- d2-admin/store/modules/header-menu.js
+++ d2-admin/store/modules/header-menu.js
@@ -1,4 +1,4 @@
 import { keyPrefix } from 'd2-projects/d2-utils/object.js'
 import { menuStore } from './menu.js'
 
-export const asideMenuStore = keyPrefix(menuStore(), 'header')
+export const headerMenuStore = () => keyPrefix(menuStore(), 'header')
