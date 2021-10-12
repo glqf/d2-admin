bcdb3c643988db7a47b2220373074d06a63f85d9
FairyEver <1711467488@qq.com>
2021-10-12 17:02:47 +0800

    docs: update

--- vite.config.js
+++ vite.config.js
@@ -1,49 +1,50 @@
 import path from 'path'
-
 import { defineConfig } from 'vite'
-
 import Vue from '@vitejs/plugin-vue'
 
+// Visualize and analyze your Rollup bundle to see which modules are taking up space
+// [website] https://github.com/btd/rollup-plugin-visualizer
 import { visualizer } from 'rollup-plugin-visualizer'
 
 // Use Markdown as Vue components
 // Use Vue components in Markdown
-// https://github.com/antfu/vite-plugin-md
+// [website] https://github.com/antfu/vite-plugin-md
 import Markdown from 'vite-plugin-md'
 
 // Provides Vue 3 JSX & TSX support with HMR
-// https://github.com/vitejs/vite/tree/main/packages/plugin-vue-jsx
+// [website] https://github.com/vitejs/vite/tree/main/packages/plugin-vue-jsx
 import Jsx from '@vitejs/plugin-vue-jsx'
 
 // On-demand components auto importing for Vue
-// https://github.com/antfu/unplugin-vue-components
+// [website] https://github.com/antfu/unplugin-vue-components
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // File system based routing for Vue 3 applications using Vite
-// https://github.com/hannoeru/vite-plugin-pages
+// [website] https://github.com/hannoeru/vite-plugin-pages
 import Pages from 'vite-plugin-pages'
 
 // Extend Vite with ability to use SVG files as Vue components
-// https://github.com/visualfanatic/vite-svg/tree/vite-plugin-svg
+// [website] https://github.com/visualfanatic/vite-svg/tree/vite-plugin-svg
 import Svg from 'vite-plugin-vue-svg'
 
 // Used to generate svg sprite map
-// https://github.com/anncwb/vite-plugin-svg-icons
+// [website] https://github.com/anncwb/vite-plugin-svg-icons
 import SvgIcons from 'vite-plugin-svg-icons'
 
 // Bundles only the icons you use
-// https://github.com/antfu/purge-icons
-// browser: https://icones.netlify.app/ or https://iconify.design/icon-sets/
-// example: <span class="iconify" data-icon="fa:home"/>
+// [website] https://github.com/antfu/purge-icons
+// [browser] https://icones.netlify.app/ or https://iconify.design/icon-sets/
+// [example] <span class="iconify" data-icon="fa:home"/>
 import PurgeIcons from 'vite-plugin-purge-icons'
 
 // Access thousands of icons as components on-demand universally
-// https://github.com/antfu/unplugin-icons
-// example: <icon-mdi-account-box/>
+// [website] https://github.com/antfu/unplugin-icons
+// [example] <icon-mdi-account-box/>
 import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
 
+// path.resolve wrapper
 const resolve = p => path.resolve(process.cwd(), p)
 
 export default defineConfig({
