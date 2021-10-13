fe3a433842e6cfd47c154e1580e12220c529eb29
FairyEver <1711467488@qq.com>
2021-10-13 13:10:06 +0800

    feat: asideMenus

--- d2-admin/components/d2-admin/layout/header-aside/menu/vertical.jsx
+++ d2-admin/components/d2-admin/layout/header-aside/menu/vertical.jsx
@@ -1,5 +1,4 @@
 import { defineComponent, reactive } from 'vue'
-import { menusAside } from '@/menus/index.js'
 import { useStore } from 'd2-admin/store/index.js'
 import { useLayoutMenu } from 'd2-admin/utils/menu.js'
 import { asideMenuStore } from 'd2-admin/store/modules/aside-menu.js'
@@ -9,16 +8,15 @@ export default defineComponent({
   setup () {
     const { asideMenus } = reactive(useStore(asideMenuStore))
 
-    console.log(asideMenus)
-
     const { onMenuSelect } = useLayoutMenu()
 
     return {
+      asideMenus,
       onMenuSelect
     }
   },
   render () {
-    return renderMenus(menusAside, {
+    return renderMenus(this.asideMenus, {
       onSelect: (index, indexPath, item, routeResult) => this.onMenuSelect(item)
     })
   }
