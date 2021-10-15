3046991e542816f9babcd47679b742602efb64ac
FairyEver <1711467488@qq.com>
2021-10-15 15:37:17 +0800

    feat: update

--- d2-admin/store/index.js
+++ d2-admin/store/index.js
@@ -1,14 +1,15 @@
 import { registerModules, getModule } from './utils/index.js'
+
+// modules
+
 import { countStore } from './modules/count.js'
 import { menuSecondaryStore } from './modules/menu-secondary.js'
 import { menuMainStore } from './modules/menu-main.js'
 
-const modules = [
+export const useStore = getModule
+
+export const creatStore = () => registerModules([
   countStore,
   menuSecondaryStore,
   menuMainStore
-]
-
-export const useStore = getModule
-
-export const creatStore = () => registerModules(modules)
+])
