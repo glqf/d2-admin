176070c5de8e9d0dfca034bb70bb3e629f04bb16
FairyEver <1711467488@qq.com>
2021-10-17 10:21:12 +0800

    feat: add pinia

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
 import App from './App.vue'
 
 import d2Admin from './plugins/d2-admin.js'
 import d2Projects from './plugins/d2-projects.js'
 
 createApp(App)
+  .use(createPinia())
   .use(d2Admin)
   .use(d2Projects)
   .mount('#app')
