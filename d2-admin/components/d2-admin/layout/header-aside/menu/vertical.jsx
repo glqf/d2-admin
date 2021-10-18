a6d8e43b7fa44b72978117ffeddde815128edf17
FairyEver <1711467488@qq.com>
2021-10-18 22:04:49 +0800

    feat: menu

--- d2-admin/components/d2-admin/layout/header-aside/menu/vertical.jsx
+++ d2-admin/components/d2-admin/layout/header-aside/menu/vertical.jsx
@@ -1,44 +1,9 @@
-import { defineComponent, computed } from 'vue'
-import { useRoute } from 'vue-router'
-import { storeToRefs } from 'pinia'
 import { useMenuMainStore } from 'd2-admin/store/menu-main.js'
-import { useMenu } from 'd2-admin/use/menu.js'
-import { getMenuId } from 'd2-admin/utils/menu.js'
-import { renderMenus } from './utils/render.jsx'
+import { defineMenuComponent } from './utils/define.jsx'
 
-export default defineComponent({
-  setup () {
-    const route = useRoute()
-
-    const menu = useMenu()
-    const { navigateByMenu } = menu
-
-    const menuMainStore = useMenuMainStore()
-    const { menus } = storeToRefs(menuMainStore)
-    const { getMenuById, getMenuByUrl } = menuMainStore
-
-    function onSelect (id) {
-      navigateByMenu(getMenuById(id))
-    }
-
-    const defaultActive = computed(() => getMenuId(getMenuByUrl(route.fullPath)))
-
-    return {
-      menus,
-      onSelect,
-      defaultActive
-    }
-  },
-  render () {
-    const {
-      menus,
-      onSelect,
-      defaultActive
-    } = this
-    return renderMenus(menus, {
-      mode: 'vertical',
-      onSelect,
-      defaultActive
-    })
+export default defineMenuComponent({
+  store: useMenuMainStore,
+  props: {
+    mode: 'vertical'
   }
 })
