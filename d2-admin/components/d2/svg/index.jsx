e96e7b2d6a7aa2e0eba85decce130dad8da6b7d1
FairyEver <1711467488@qq.com>
2021-10-21 11:23:19 +0800

    feat: iconName

--- d2-admin/components/d2/svg/index.jsx
+++ d2-admin/components/d2/svg/index.jsx
@@ -1,5 +1,5 @@
 import makeClassnames from 'classnames'
-import { defineComponent, ref, unref, watch, computed } from 'vue'
+import { defineComponent, unref, computed } from 'vue'
 import { useConfig } from 'd2-projects/d2-config/index.js'
 import { makeComponentName, makeComponentClassName } from 'd2-projects/d2-utils/special/d2-components/name.js'
 
@@ -23,13 +23,13 @@ export default defineComponent({
     const dir = computed(() => props.dir || svgDir)
 
     const href = computed(() => {
-      let result = unref(symbolId)
+      let iconName = '#' + unref(symbolId)
         .replace(/\[dir\]/g, unref(dir))
         .replace(/\[name\]/g, props.name)
       if (!unref(dir)) {
-        result = result.replace('--', '-')
+        iconName = iconName.replace('--', '-')
       }
-      return '#' + result
+      return iconName
     })
 
     const classnames = computed(() => makeClassnames(classname, {}))
