34d12de7646582cb976a1f05dd3916981fd4d8f7
FairyEver <1711467488@qq.com>
2021-10-21 08:36:59 +0800

    feat: icon style

--- d2-admin/components/d2/icon/index.jsx
+++ d2-admin/components/d2/icon/index.jsx
@@ -5,6 +5,8 @@ import { clearElement } from 'd2-projects/d2-utils/dom.js'
 import { useConfig } from 'd2-projects/d2-config/index.js'
 import { makeComponentName, makeComponentClassName } from 'd2-projects/d2-utils/special/d2-components/name.js'
 
+import './index.scss'
+
 const name = 'icon'
 
 const componentName = makeComponentName(name)
