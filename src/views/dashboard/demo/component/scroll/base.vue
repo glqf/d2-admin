c5cfb36fbb4443328ffedf83ab69d20979f822dd
FairyEver <1711467488@qq.com>
2021-10-21 14:55:19 +0800

    feat: d2-scroll--demo

--- src/views/dashboard/demo/component/scroll/base.vue
+++ src/views/dashboard/demo/component/scroll/base.vue
@@ -1,11 +1,21 @@
 <style lang="scss" scoped>
 .d2-scroll--demo {
-  @apply w-64 h-64 border border-blue-500 rounded;
+  height: 200px;
+  width: 200px;
+  @apply border border-gray-800 rounded;
+  .row {
+    @apply px-4 py-2 bg-gray-700 text-gray-100 border-b border-gray-800;
+    &:last-child {
+      @apply border-b-0;
+    }
+  }
 }
 </style>
 
 <template>
   <d2-scroll class="d2-scroll--demo">
-    <p v-for="n in 100" :key="n">n: {{ n }}</p>
+    <p class="row" v-for="n in 100" :key="n">
+      n: {{ n }}
+    </p>
   </d2-scroll>
 </template>
