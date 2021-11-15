649303a07ebe27ae7422749def58e030ed83d9ce
FairyEver <1711467488@qq.com>
2021-11-15 21:34:43 +0800

    feat: collapsed

--- d2/components/d2/admin/layout/dashboard/store/index.js
+++ d2/components/d2/admin/layout/dashboard/store/index.js
@@ -3,7 +3,7 @@ import { defineStore } from 'pinia'
 export const useD2AdminLayoutDashboardStore = defineStore('d2-admin-layout-dashboard', {
   state: () => {
     return {
-      folde: false
+      collapsed: false
     }
   }
 })
