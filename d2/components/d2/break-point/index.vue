220e98e699efbcaf552c650961250159e8d30e5e
FairyEver <1711467488@qq.com>
2021-12-03 20:47:48 +0800

    refactor: slot

--- d2/components/d2/break-point/index.vue
+++ d2/components/d2/break-point/index.vue
@@ -1,18 +1,12 @@
 <template>
-  <slot v-bind="{ ...status, data }"/>
-  <slot
-    v-for="breakPoint in breakPointsName"
-    :key="breakPoint"
-    :name="breakPoint"
-    v-bind="{ ...status, data }"/>
+  <slot v-bind="scope"/>
 </template>
 
 <script>
 import { unref, reactive, defineComponent, computed } from 'vue'
-import { keys, mapValues } from 'lodash-es'
+import { mapValues } from 'lodash-es'
 import { makeName } from 'd2/utils/component.js'
 import { useBreakPoint } from 'd2/use/break-point.js'
-import { useConfig } from 'd2/components/d2/config/use.js'
 
 const name = 'break-point'
 
@@ -37,45 +31,20 @@ export default defineComponent({
     breakPoints: { type: Object, default: () => ({}) }
   },
   setup (props) {
-    const breakPoints = Object.assign(
-      {},
-      useConfig().breakPoints,
-      props.breakPoints
-    )
+    const breakPoint = useBreakPoint(props.breakPoints)
 
-    const breakPointsName = keys(breakPoints)
-    
-    const status = reactive(useBreakPoint(breakPoints))
+    const { responsive } = breakPoint
 
-    const data = mapValues(
-      props.data,
-      (_, k) => unref(status.responsive(...props.data[k]))
-    )
+    const data = mapValues(props.data, (_, k) => unref(responsive(...props.data[k])))
+
+    const scope = computed(() => ({
+      ...reactive(breakPoint),
+      data
+    }))
     
     return {
-      breakPoints,
-      breakPointsName,
-      status,
-      data
+      scope
     }
   }
-  // render () {
-  //   const {
-  //     breakPoints,
-  //     status,
-  //     data
-  //   } = this
-  //   const scope = {
-  //     ...reactive(status),
-  //     data
-  //   }
-  //   const slot = name => this.$slots?.default?.(scope)
-  //   return [
-  //     slot('default'),
-  //     slot('min'),
-  //     ...keys(breakPoints)
-  //       .map(e => slot(e))
-  //   ]
-  // }
 })
 </script>
