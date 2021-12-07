38ae679c7d44ab269f2cf3ad7d0c582c62434df1
FairyEver <1711467488@qq.com>
2021-12-07 09:04:17 +0800

    feat: demos

--- d2/views/dashboard/demo/layout/dashboard/custom.vue
+++ d2/views/dashboard/demo/layout/dashboard/custom.vue
@@ -8,14 +8,44 @@
 
 <template>
   <d2-admin-layout-dashboard-container>
-    <template #header>
+    <template v-if="headeractive" #header>
       <a-button>Button</a-button>
     </template>
-    <template #footer>
+    <template v-if="footeractive" #footer>
       <a-button>Button</a-button>
     </template>
-    <ul>
-      <li v-for="n in 100" :key="n">{{ n }}</li>
-    </ul>
+    <section class="bg-gray-100 border border-gray-200 rounded p-4 mb-4">
+      <a-space>
+        <a-button @click="headerToggle">切换 Header</a-button>
+        <a-button @click="footerToggle">切换 Footer</a-button>
+      </a-space>
+    </section>
+    <demo-markdown-article/>
   </d2-admin-layout-dashboard-container>
 </template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    const headeractive = ref(true)
+    const footeractive = ref(true)
+
+    function headerToggle () {
+      headeractive.value = !headeractive.value
+    }
+
+    function footerToggle () {
+      footeractive.value = !footeractive.value
+    }
+
+    return {
+      headeractive,
+      footeractive,
+      headerToggle,
+      footerToggle
+    }
+  }
+}
+</script>
