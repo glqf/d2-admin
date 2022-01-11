cefb806385c5d7b3cf0382d36e0e822a2ca2b6d0
FairyEver <1711467488@qq.com>
2022-01-11 14:56:00 +0800

    docs: hideSurelyTableWatermark doc

--- d2/utils/helper/surely-vue.js
+++ d2/utils/helper/surely-vue.js
@@ -1,4 +1,21 @@
+// ---------- Surely Vue ----------
+// [website]  https://www.surely.cool
+// [purchase] https://www.surely.cool/pricing
+
+// 声明
+// 1. 去除水印的方法仅支持在开发阶段使用
+// 2. 建议联系 Surely Vue 客服获取用于开发环境的临时激活码
+// 3. 如果您自行选择在已经上线的项目中使用本项目中的方法去除水印造成的一切后果只能您自行承担
+// 4. D2Admin (https://github.com/d2-projects/d2-admin) 没有在任何生产环境下使用此方法隐藏水印
+
+// Statement
+// 1. The method of removing watermark is only supported in the development stage
+// 2. It is recommended that you contact surely Vue's guest salesperson to obtain a temporary activation code for use in the development environment
+// 3. If you choose to use the method in this project to remove the watermark in the online project, you can only bear all the consequences
+// 4. D2Admin (https://github.com/d2-projects/d2-admin) does not use this method to hide watermarks in any production environment
+
 export function hideSurelyTableWatermark () {
+  if (import.meta.env.PROD) return
   if (location.hostname !== 'localhost') return
   document.querySelectorAll('div.surely-table > div.surely-table-body > div')
     .forEach(e => e.innerHTML === 'Powered by Surely Vue' && (e.innerHTML = ''))
